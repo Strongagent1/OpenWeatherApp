cc54fffa6eca8f4311673ca0c9ad67dc0beb1657
kyle jenne <strongagent1@gmail.com>
2022-11-21 11:44:14 -0500

    openweather exercise for KJ

--- OpenWeatherApp/Program.cs
+++ OpenWeatherApp/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace OpenWeatherApp
+﻿using System.Text.Json.Nodes;
+using Newtonsoft.Json.Linq;
+
+namespace OpenWeatherApp
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string key = File.ReadAllText("appsettings.json");
+            var apiKey = JObject.Parse(key).GetValue("DefaultKey").ToString();
+
+            Console.WriteLine("Pleae entet a zip code where you would like to check the weather.");
+            var zipCode = Console.ReadLine();
+
+            var apiCall = $"https://api.openweathermap.org/data/2.5/weather?zip={zipCode}&appid={apiKey}&units=imperial";
+            
+            Console.WriteLine($"Thank you the current temperature is {WeatherMap.GetTemp(apiCall)}F");
         }
     }
 }
